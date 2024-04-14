06953a1452181f340c73530c87b08d1269daf126
Lezoson <[lezoson@outlook.com]>
2024-04-15 00:40:21 +0530

    Feat : Added Chosen

--- StudentsMangement/obj/Debug/net8.0/StudentsMangement.AssemblyInfo.cs
+++ StudentsMangement/obj/Debug/net8.0/StudentsMangement.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentsMangement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01c7fe88164c18d1d245f3579e2412e9631a7bbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f792f262a3d8ee1783eddad3af011b72a8dc765")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentsMangement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentsMangement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
